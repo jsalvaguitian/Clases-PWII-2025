40c4cc6de4174854e9c2508d3389f829f5022b5a
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2025-04-17 23:17:08 -0300

    formulario con metodo post y php

--- clase-25-04-01/action.php
+++ clase-25-04-01/action.php
@@ -1,9 +1,33 @@
 <?php
 /*para obtener el valor de la variable que fue enviada por la url */
-if(isset($_GET['nombre'])){
+/*if(isset($_GET['nombre'])){
     echo "Nombre: ".$_GET['nombre'];
 
 }else{
     echo "Up! no existe la variable 'nombre'";
+
+}*/
+
+if(isset($_POST['nombre'])){
+    echo "Nombre: ".$_POST['nombre']."</br>";
+
+}else{
+    echo "Up! no existe la variable 'nombre'";
+
+}
+
+if(isset($_POST['email'])){
+    echo "Email: ".$_POST['email']."</br>";
+
+}else{
+    echo "Up! no existe la variable 'email'";
+
+}
+
+if(isset($_POST['fecha'])){
+    echo "Fecha: ".$_POST['fecha'];
+
+}else{
+    echo "Up! no existe la variable 'fecha'";
 
 }
